de18fa02484d4e2ba7ff8138ac81c4d301ed2cc5
Hamdrive <hamza.husein017@gmail.com>
2022-02-04 20:26:37 +0530

    [feat][refactor] landing and getstarted page

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const closeMobNav = document.querySelector(".nav-close-btn");
 const mobNav = document.querySelector(".mobile-navbar");
-const mobNavMenu = document.querySelector(".menu");
-const menuBtns = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
-  console.log("click");
-  mobNav.style.width = "50vw";
+  mobNav.style.width = "60vw";
 });
 
 closeMobNav.addEventListener("click", () => {
-  mobNav.style.width = "0";
+  mobNav.style.width = null;
 });
