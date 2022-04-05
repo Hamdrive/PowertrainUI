ce7881783bed2def0ecb21d5ac013b9ac1fb7b2a
Hamdrive <hamza.husein017@gmail.com>
2022-04-05 06:53:01 +0530

    [feat] mobile navigation example

---
+++ Documentation/js/mobile-nav.js
@@ -0,0 +1,17 @@
+const hamburger_mobile = document.getElementsByClassName("hamburger")[1];
+const close_sidemenu = document.getElementsByClassName(
+  "mobile-sidebar-close-btn"
+)[0];
+const sidemenu = document.getElementsByClassName("mobile-sidebar")[0];
+
+hamburger_mobile.addEventListener("click", () => {
+  console.log("click");
+  sidemenu.style.width = "50%";
+  sidemenu.style.overflow = "auto"
+});
+
+close_sidemenu.addEventListener("click", () => {
+  console.log("click");
+  sidemenu.style.width = "0";
+  sidemenu.style.overflow = "hidden"
+});
